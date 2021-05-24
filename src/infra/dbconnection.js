74f1913c4d512812e21aa1a8dd808e1805caccc3
vinicius-linux <viniciusalmeidarodrigues96@gmail.com>
2021-05-24 10:52:24 -0300

    Atualização credenciais do banco de dados

--- src/infra/dbconnection.js
+++ src/infra/dbconnection.js
@@ -3,9 +3,9 @@ const mysql = require('mysql')
 const connection = mysql.createConnection({
     host: 'sql10.freemysqlhosting.net',
     port: '3306',
-    user: 'sql10412198',
-    password: 'Sa1TVA6CaE',
-    database: 'sql10412198'
+    user: 'sql10414543',
+    password: 'z5ZFvxZlIr',
+    database: 'sql10414543'
 })
 
 
